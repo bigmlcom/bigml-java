ce0fedd645bf5bf5edb115e7802c91af24fcd775
Javier García <javi@MacBook-Air-de-Javier.local>
2012-09-27 19:26:04 +0200

    New utility method to get values in JSONObject

--- src/main/java/org/bigml/binding/utils/Utils.java
+++ src/main/java/org/bigml/binding/utils/Utils.java
@@ -7,6 +7,8 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 
 public class Utils {
@@ -43,6 +45,43 @@ public class Utils {
     	return false;
     }
   }
-  
+
+
+  /**
+   * Returns JSONObject child.
+   *
+   */
+  public static Object getJSONObject(JSONObject json, String path) {
+    String field = path;
+    if (path.indexOf(".")!=-1) {
+      field = path.substring(0, path.indexOf("."));
+    }
+	 
+    if (json.get(field) instanceof JSONArray) {
+      return (JSONArray) json.get(field);
+    }
+    if (json.get(field) instanceof String) {
+      return (String) json.get(field);
+    }
+    if (json.get(field) instanceof Long) {
+      return (Long) json.get(field);
+    }
+    json = (JSONObject) json.get(field);
+	
+    if (json==null) {
+      return null;
+    }
+
+    if (path.indexOf(".")==-1) {
+      return json;
+    }
+
+    path = path.substring(path.indexOf(".")+1, path.length());
+    if (path.length()>0) {
+      return getJSONObject(json, path);
+    }
+
+    return null;
+  }
 	
 }
