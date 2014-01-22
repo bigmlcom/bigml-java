5a1bcc3702c3ec7a20a449af2a49714df75a289f
Jose Antonio Ortega Ruiz <jao@gnu.org>
2014-01-22 05:21:14 +0100

    LocalEnsembles cleanups

--- src/main/java/org/bigml/binding/LocalEnsemble.java
+++ src/main/java/org/bigml/binding/LocalEnsemble.java
@@ -1,34 +1,36 @@
 /*
- An local Ensemble object.
+  An local Ensemble object.
 
-This module defines an Ensemble to make predictions locally using its
-associated models.
+  This module defines an Ensemble to make predictions locally using its
+  associated models.
 
-This module can not only save you a few credits, but also enormously
-reduce the latency for each prediction and let you use your models
-offline.
+  This module can not only save you a few credits, but also enormously
+  reduce the latency for each prediction and let you use your models
+  offline.
 
-import org.bigml.binding.BigMLClient;
-import org.bigml.binding.resources.Ensemble;
+  import org.bigml.binding.BigMLClient;
+  import org.bigml.binding.resources.Ensemble;
 
 
-# creating ensemble
-Ensemble ensemble = BigMLCliente.getInstance().createEnsemble('dataset/5143a51a37203f2cf7000972')
+  # creating ensemble
+  Ensemble ensemble = BigMLClient.getInstance().createEnsemble('dataset/5143a51a37203f2cf7000972')
 
-# Ensemble object to predict
-LocalEnsemble localEnsemble = LocalEnsemble(ensemble, storage='./storage')
-localEnsemble.predict({"petal length": 3, "petal width": 1})
- */
+  # Ensemble object to predict
+  LocalEnsemble localEnsemble = LocalEnsemble(ensemble)
+  localEnsemble.predict("{\"petal length\": 3, \"petal width\": 1}")
+*/
 
 package org.bigml.binding;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.bigml.binding.utils.Utils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 /**
  * A local predictive Ensemble.
@@ -39,79 +41,59 @@ import org.json.simple.JSONObject;
  */
 public class LocalEnsemble {
 
-	/**
-	 * Logging
-	 */
-	static Logger logger = Logger.getLogger(LocalEnsemble.class.getName());
+    /**
+     * Logging
+     */
+    static Logger logger = Logger.getLogger(LocalEnsemble.class.getName());
+
+    private String ensembleId;
+
+    private String[] modelsIds;
+    private JSONArray models;
+    private MultiModel multiModel;
 
 
-	private String storage;
-	private String ensembleId;
+    /**
+     * Constructor
+     *
+     * @param ensemble  the json representation for the remote ensemble
+     */
+    public LocalEnsemble(JSONObject ensemble) throws Exception {
 
-	private String[] modelsIds;
-	private String[] modelsSplit;
-	private JSONArray models;
-	private MultiModel multiModel;
+        if (ensemble.get("objects")!=null) {
+            throw new IllegalArgumentException("Embedded objects unsupported");
+        } else {
+            this.ensembleId = (String) ensemble.get("resource");
+            JSONArray modelsJson =
+                (JSONArray) Utils.getJSONObject(ensemble, "object.models");
+            int mn = modelsJson.size();
+            modelsIds = (String[]) new String[mn];
+            for (int i=0 ; i < mn; i++) {
+                modelsIds[i] = (String) modelsJson.get(i);
+            }
+        }
 
+        BigMLClient bigmlClient = BigMLClient.getInstance();
+        models = new JSONArray();
 
-	/**
-	 * Constructor
-	 *
-	 * @param ensemble	the json representation for the remote ensemble
-	 */
-	public LocalEnsemble(JSONObject ensemble, final String storage, final Integer maxModels) throws Exception {
-		super();
+        for (String id : modelsIds) {
+            models.add(bigmlClient.getModel(id));
+        }
+        multiModel = new MultiModel(models);
+    }
 
-		this.storage = storage!=null ? storage : "./storage";
-		this.ensembleId = null;
+    /**
+     * Accessor to the full list of fields used by this ensemble.
+     * It's obtained from the first model in the ensemble.
+     */
+    public JSONObject getFields() {
+        JSONObject model = (JSONObject) this.models.get(0);
+        return (JSONObject) Utils.getJSONObject(model, "object.model.fields");
+    }
 
-		if (ensemble.get("objects")!=null) {
-			try {
-				// TODO: list of models
-				// models = [get_model_id(model) for model in ensemble]
-			} catch (Exception e) {
-				logger.error("'Failed to verify the list of models. Check your model id values", e);
-				throw new Exception();
-			}
-		} else {
-			this.ensembleId = (String) ensemble.get("resource");
-			JSONArray modelsJson = (JSONArray) Utils.getJSONObject(ensemble, "object.models");
-			modelsIds = (String[]) new String[modelsJson.size()];
-			for (int i=0; i<modelsJson.size(); i++) {
-				modelsIds[i] = (String) modelsJson.get(i);
-			}
-		}
-
-		int numberOfModels = modelsIds.length;
-		if (maxModels == null) {
-			modelsSplit = modelsIds;
-		} else {
-			int maxLength = numberOfModels>maxModels ? maxModels : numberOfModels;
-			modelsSplit = Arrays.copyOfRange(modelsIds, 0, maxLength);
-		}
-
-		BigMLClient bigmlClient = BigMLClient.getInstance(storage);
-		models = new JSONArray();
-
-		for (int i=0; i<modelsSplit.length; i++) {
-			String modelId = (String) modelsSplit[i];
-			models.add(bigmlClient.getModel(modelId));
-		}
-		multiModel = new MultiModel(models);
-	}
-
-
-	/**
-	 * Lists all the model/ids that compound the ensemble.
-	 */
-	public String[] listModels() {
-		return this.modelsIds;
-	}
-
-
-	/**
-	 * Makes a prediction based on the prediction made by every model.
-	 *
+    /**
+     * Makes a prediction based on the prediction made by every model.
+     *
      * The method parameter is a numeric key to the following combination
      * methods in classifications/regressions:
      *    0 - majority vote (plurality)/ average: PLURALITY_CODE
@@ -119,21 +101,36 @@ public class LocalEnsemble {
      *          CONFIDENCE_CODE
      *    2 - probability weighted majority vote / average:
      *          PROBABILITY_CODE
-	 */
-	public HashMap<Object, Object> predict(final String inputData, Boolean byName, Integer method, Boolean withConfidence) throws Exception {
-		if (method == null) {
-			method = MultiVote.PLURALITY;
-		}
-		if (byName == null) {
-			byName = true;
-		}
-		if (withConfidence == null) {
-			withConfidence = false;
-		}
+     */
+    public Map<Object, Object> predict(final String inputData,
+                                       Boolean byName,
+                                       Integer method,
+                                       Boolean withConfidence)
+        throws Exception {
+        if (method == null) {
+            method = MultiVote.PLURALITY;
+        }
+        if (byName == null) {
+            byName = true;
+        }
+        if (withConfidence == null) {
+            withConfidence = false;
+        }
 
-		MultiVote votes = this.multiModel.generateVotes(inputData, byName, withConfidence);
-		HashMap<Object, Object> combinedPrediction = votes.combine(method, withConfidence);
+        MultiVote votes =
+            this.multiModel.generateVotes(inputData, byName, withConfidence);
+        return  votes.combine(method, withConfidence);
+    }
 
-	    return combinedPrediction;
-	}
+    /**
+     * Convenience version of predict that take as inputs a map from
+     * field ids or names to their values as Java objects.  See also
+     * predict(String, Boolean, Integer, Boolean).
+     */
+    public Map<Object, Object> predict(Map<Object, Object> inputs,
+                                       Boolean byName,
+                                       Integer method,
+                                       Boolean conf) throws Exception {
+        return predict(JSONValue.toJSONString(inputs), byName, method, conf);
+    }
 }
