b53bd08470c210b683f5a26dd6a7bda177f9ef20
Javier Alperte <alperte@bigml.com>
2015-03-05 08:51:45 -0300

    Add support for Last Prediction and Proportional strategies for missing splits

--- src/main/java/org/bigml/binding/LocalPredictiveModel.java
+++ src/main/java/org/bigml/binding/LocalPredictiveModel.java
@@ -33,12 +33,13 @@
  */
 package org.bigml.binding;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.DecimalFormat;
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.*;
 
 import org.bigml.binding.localmodel.Tree;
 import org.bigml.binding.utils.Utils;
@@ -55,7 +56,7 @@ import org.slf4j.LoggerFactory;
  * generate prediction locally.
  * 
  */
-public class LocalPredictiveModel {
+public class LocalPredictiveModel implements PredictionConverter {
 
     /**
      * Logging
@@ -153,7 +154,8 @@ public class LocalPredictiveModel {
             // Check duplicated field names
             uniquifyVarnames();
 
-            this.tree = new Tree(root, this.fields, objectiveField);
+            this.tree = new Tree(root, this.fields, objectiveField,
+                    (JSONObject) Utils.getJSONObject(model, prefix + "model.distribution.training"));
         } catch (Exception e) {
             logger.error("Invalid model structure", e);
             throw new InvalidModelException();
@@ -212,7 +214,10 @@ public class LocalPredictiveModel {
     }
 
     /**
-     * Makes a prediction based on a number of field values.
+     * Makes a prediction based on a number of field values using a Last Prediction Strategy
+     *
+     * By default the input fields must be keyed by field name but you can use `by_name`
+     *  to input them directly keyed by id.
      * 
      */
     @Deprecated
@@ -231,32 +236,113 @@ public class LocalPredictiveModel {
         }
         JSONObject inputData = argsData;
 
-        if (!byName) {
-            inputData = new JSONObject();
-            Iterator iter = argsData.keySet().iterator();
-            while (iter.hasNext()) {
-                String key = (String) iter.next();
-                String fieldName = (String) Utils.getJSONObject(fields, key
-                        + ".name");
-                inputData.put(fieldName, argsData.get(key));
-            }
-        }
 
-        return tree.predict(inputData, withConfidence);
+        return predict(inputData, byName, withConfidence);
     }
 
     /**
-     * Makes a prediction based on a number of field values.
+     * Makes a prediction based on a number of field values using a Last Prediction Strategy
      * 
      */
     public HashMap<Object, Object> predict(final JSONObject args,
             Boolean byName, Boolean withConfidence)
             throws InputDataParseException {
+      return predict(args, byName, null, null, withConfidence, MissingStrategy.LAST_PREDICTION,
+              null, null, null, null, null, null).get(0);
+    }
+
+    /**
+     * Makes a prediction based on a number of field values using a Last Prediction Strategy
+     *
+     */
+    public HashMap<Object, Object> predict(final JSONObject args,
+            Boolean byName, MissingStrategy strategy, Boolean withConfidence)
+            throws InputDataParseException {
+      return predict(args, byName, null, null, withConfidence, strategy,
+              null, null, null, null, null, null).get(0);
+    }
+
+    /**
+     * Makes a prediction based on a number of field values.
+     *
+     * By default the input fields must be keyed by field name but you can use
+     *  `byName` to input them directly keyed by id.
+     *
+     * inputData: Input data to be predicted
+     * byName: Boolean, True if input_data is keyed by names
+     * printPath: Boolean, if True the rules that lead to the prediction
+     *              are printed
+     * out: output handler
+     * withConfidence: Boolean, if True, all the information in the node
+     *                  (prediction, confidence, distribution and count)
+     *                  is returned in a list format
+     * missingStrategy: LAST_PREDICTION|PROPORTIONAL missing strategy for
+     *                  missing fields
+     * addConfidence: Boolean, if True adds confidence to the dict output
+     * addPath: Boolean, if True adds path to the dict output
+     * addDistribution: Boolean, if True adds distribution info to the
+     *                      dict output
+     * addCount: Boolean, if True adds the number of instances in the
+     *                      node to the dict output
+     * addMedian: Boolean, if True adds the median of the values in
+     *                      the distribution
+     * multiple: For categorical fields, it will return the categories
+     *           in the distribution of the predicted node as a
+     *      list of dicts:
+     *          [{'prediction': 'Iris-setosa',
+     *              'confidence': 0.9154
+     *              'probability': 0.97
+     *              'count': 97},
+     *           {'prediction': 'Iris-virginica',
+     *              'confidence': 0.0103
+     *              'probability': 0.03,
+     *              'count': 3}]
+     * The value of this argument can either be an integer
+     *  (maximum number of categories to be returned), or the
+     *  literal 'all', that will cause the entire distribution
+     *  in the node to be returned.
+     *
+     */
+    public List<HashMap<Object, Object>> predict(final JSONObject args,
+            Boolean byName, Boolean printPath, OutputStream out,
+            Boolean withConfidence, MissingStrategy strategy,
+            Boolean addConfidence, Boolean addPath, Boolean addDistribution,
+            Boolean addCount, Boolean addMedian, Object multiple)
+            throws InputDataParseException {
+
+        List<HashMap<Object, Object>> outputs = new ArrayList<HashMap<Object, Object>>();
+
         if (byName == null) {
             byName = true;
         }
+        if (printPath == null) {
+            printPath = false;
+        }
+        if (out == null) {
+            out = System.out;
+        }
         if (withConfidence == null) {
             withConfidence = false;
+        }
+
+        if (strategy == null) {
+            strategy = MissingStrategy.LAST_PREDICTION;
+        }
+
+        if (addPath == null) {
+            addPath = false;
+        }
+        if (addConfidence == null) {
+            addConfidence = false;
+        }
+        if (addDistribution == null) {
+            addDistribution = false;
+        }
+        if (addCount == null) {
+            addCount = false;
+        }
+        if (addMedian == null) {
+            addMedian = false;
         }
 
         if (args == null) {
@@ -264,6 +350,11 @@ public class LocalPredictiveModel {
         }
         JSONObject inputData = args;
 
+
+        // Checks and cleans inputData leaving the fields used in the model
+        inputData = filterInputData(inputData, byName);
+
+        // Lets work always using the field names instead of the keys
         if (!byName) {
             inputData = new JSONObject();
             Iterator iter = args.keySet().iterator();
@@ -275,7 +366,99 @@ public class LocalPredictiveModel {
             }
         }
 
-        return tree.predict(inputData, withConfidence);
+        Integer multipleNum = null;
+        Boolean multipleAll = null;
+
+        if( multiple != null ) {
+            if( multiple instanceof String ) {
+                if( "all".equals(multiple)  ) {
+                    multipleAll = true;
+                } else {
+                    throw new IllegalArgumentException("The value of the \"multiple\"" +
+                            " argument  can either be an integer" +
+                            " (maximum number of categories to be returned), or the" +
+                            " literal 'all', that will cause the entire distribution" +
+                            " in the node to be returned.");
+                }
+            } else if( multiple instanceof Number ) {
+                multipleNum = ((Number) multiple).intValue();
+            }
+        }
+
+        // Strips affixes for numeric values and casts to the final field type
+        Utils.cast(inputData, fields);
+
+        HashMap<Object, Object> predictionInfo = tree.predict(inputData, null, strategy);
+
+        Object prediction = predictionInfo.get("prediction");
+        List<String> path = (List<String>) predictionInfo.get("path");
+        Double confidence = (Double) predictionInfo.get("confidence");
+        List<JSONArray>  distribution = (List<JSONArray>) predictionInfo.get("distribution");
+        Long instances = (Long) predictionInfo.get("instances");
+        Double median = (Double) predictionInfo.get("median");
+        Object distribution_unit = predictionInfo.get("distribution_unit");
+
+        if( printPath ) {
+            try {
+                out.write((Utils.join(path, " AND ") + " => " + prediction + "\n").getBytes());
+                out.flush();
+            } catch (IOException e) {
+                logger.error("Unable to print the path into the outputstream.", e);
+            }
+        }
+
+        HashMap<Object, Object> output = new HashMap<Object, Object>();
+        output.put("prediction", prediction);
+
+        if( withConfidence ) {
+            output.put("confidence", confidence);
+            output.put("distribution", distribution);
+            output.put("instances", instances);
+        }
+
+        if( multiple != null && !tree.isRegression() ) {
+            Long totalInstances = instances;
+
+            for( int iDistIndex = 0; iDistIndex < distribution.size(); iDistIndex++ ) {
+                JSONArray distElement = distribution.get(iDistIndex);
+                if( multipleAll || iDistIndex < multipleNum ) {
+                    predictionInfo = new HashMap<Object, Object>();
+                    // Category
+                    Object category = distElement.get(0);
+                    predictionInfo.put("prediction", category);
+                    predictionInfo.put("confidence",
+                            Tree.wsConfidence(category, distribution));
+                    predictionInfo.put("probability",
+                            ((Integer) distElement.get(1)) / totalInstances);
+                    predictionInfo.put("count", distElement.get(1));
+
+                    outputs.add(predictionInfo);
+                }
+            }
+
+            return outputs;
+        } else {
+            if( addConfidence ) {
+                output.put("confidence", confidence);
+            }
+            if( addPath ) {
+                output.put("path", new ArrayList<String>(path));
+            }
+            if( addDistribution ) {
+                output.put("distribution", distribution);
+                output.put("distribution_unit", distribution_unit);
+            }
+            if( addCount ) {
+                output.put("count", instances);
+            }
+            if( tree.isRegression() && addMedian ) {
+                output.put("median", median);
+            }
+
+            outputs.add(output);
+
+            return outputs;
+        }
     }
 
     /**
@@ -284,23 +467,34 @@ public class LocalPredictiveModel {
      * Boolean, Integer, Boolean).
      */
     public HashMap<Object, Object> predictWithMap(
-            final Map<String, Object> inputs, Boolean byName, Boolean conf)
+            final Map<String, Object> inputs, Boolean byName, Boolean withConfidence)
             throws InputDataParseException {
 
         JSONObject inputObj = (JSONObject) JSONValue.parse(JSONValue
                 .toJSONString(inputs));
-        return predict(inputObj, byName, conf);
+        return predict(inputObj, byName, null, null, withConfidence, MissingStrategy.LAST_PREDICTION,
+                null, null, null, null, null, null).get(0);
+    }
+
+    public HashMap<Object, Object> predictWithMap(
+            final Map<String, Object> inputs, Boolean byName, Boolean withConfidence, MissingStrategy missingStrategy)
+            throws InputDataParseException {
+
+        JSONObject inputObj = (JSONObject) JSONValue.parse(JSONValue
+                .toJSONString(inputs));
+        return predict(inputObj, byName, null, null, withConfidence, missingStrategy,
+                null, null, null, null, null, null).get(0);
     }
 
     public HashMap<Object, Object> predictWithMap(
             final Map<String, Object> inputs, Boolean byName)
             throws InputDataParseException {
-        return predictWithMap(inputs, byName, null);
+        return predictWithMap(inputs, byName, null, MissingStrategy.LAST_PREDICTION);
     }
 
     public HashMap<Object, Object> predictWithMap(
             final Map<String, Object> inputs) throws InputDataParseException {
-        return predictWithMap(inputs, null, null);
+        return predictWithMap(inputs, null, null, MissingStrategy.LAST_PREDICTION);
     }
 
     /**
@@ -311,6 +505,58 @@ public class LocalPredictiveModel {
      */
     public String rules(final int depth) {
         return tree.rules(depth);
+    }
+
+
+    /**
+     * Given a prediction string, returns its value in the required type
+     *
+     * @param valueAsString the prediction value as string
+     * @return
+     */
+    public Object toPrediction(String valueAsString, Locale locale) {
+        locale = (locale != null ? locale : BigMLClient.DEFAUL_LOCALE);
+
+        String objectiveFieldName = tree.getObjectiveField();
+        if( "numeric".equals(Utils.getJSONObject(fields, objectiveFieldName + ".optype")) ) {
+            String dataTypeStr = (String) Utils.getJSONObject(fields, objectiveFieldName + ".'datatype'");
+            DataTypeEnum dataType = DataTypeEnum.valueOf(dataTypeStr.toUpperCase().replace("-",""));
+            switch (dataType) {
+                case DOUBLE:
+                case FLOAT:
+                    try {
+                        NumberFormat formatter = DecimalFormat.getInstance(locale);
+                        return formatter.parse(valueAsString).doubleValue();
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                        return valueAsString;
+                    }
+
+                case INTEGER:
+                case INT8:
+                case INT16:
+                case INT32:
+                case INT64:
+                case DAY:
+                case MONTH:
+                case YEAR:
+                case HOUR:
+                case MINUTE:
+                case SECOND:
+                case MILLISECOND:
+                case DAYOFMONTH:
+                case DAYOFWEEK:
+                    try {
+                        NumberFormat formatter = NumberFormat.getInstance(locale);
+                        return formatter.parse(valueAsString).longValue();
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                        return valueAsString;
+                    }
+            }
+        }
+
+        return valueAsString;
     }
 
     /**
@@ -398,4 +644,52 @@ public class LocalPredictiveModel {
         }
     }
 
+    /**
+     * Filters the keys given in inputData checking against model fields
+     *
+     * @param inputData
+     * @param byName
+     * @return
+     */
+    public JSONObject filterInputData(JSONObject inputData, boolean byName) {
+
+        List<String> emptyFields = new ArrayList<String>();
+        for (Object key : inputData.keySet()) {
+            if( inputData.get(key) == null ) {
+                emptyFields.add(key.toString());
+            }
+        }
+
+        for (String emptyField : emptyFields) {
+            inputData.remove(emptyField);
+        }
+
+        JSONObject newInputData = new JSONObject();
+
+        if( byName ) {
+            JSONObject invertedFields = Utils.invertDictionary(fields);
+            for (Object inputDataKey : inputData.keySet()) {
+                if( invertedFields.containsKey(inputDataKey) ) {
+                    newInputData.put(inputDataKey, inputData.get(inputDataKey));
+                }
+            }
+        } else {
+            for (Object inputDataKey : inputData.keySet()) {
+                if( fields.containsKey(inputDataKey) ) {
+                    newInputData.put(inputDataKey, inputData.get(inputDataKey));
+                }
+            }
+        }
+
+        return newInputData;
+    }
+
+
+    private enum DataTypeEnum {
+        DOUBLE, FLOAT, INTEGER,
+        INT8, INT16, INT32, INT64,
+        DAY, MONTH, YEAR,
+        HOUR, MINUTE, SECOND, MILLISECOND,
+        DAYOFWEEK, DAYOFMONTH
+    }
 }
