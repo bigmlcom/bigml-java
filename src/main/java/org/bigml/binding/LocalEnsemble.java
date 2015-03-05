489a22c8d5c9876f84153af8ace0a0244c1232d1
Javier Alperte <alperte@bigml.com>
2015-03-05 08:49:37 -0300

    Support for LocalEnsemble using splits of models and a list of models. Add support to get the fields importance information.

--- src/main/java/org/bigml/binding/LocalEnsemble.java
+++ src/main/java/org/bigml/binding/LocalEnsemble.java
@@ -22,8 +22,9 @@
 
 package org.bigml.binding;
 
-import java.util.Map;
+import java.util.*;
 
+import org.bigml.binding.resources.EnsemblePredictionMethod;
 import org.bigml.binding.utils.Utils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -49,15 +50,22 @@ public class LocalEnsemble {
 
     private String[] modelsIds;
     private final JSONArray models;
+    private JSONObject fields;
+    private Map<String, String> fieldNames = new HashMap<String, String>();
+
+    private final List<JSONArray> models_split = new ArrayList<JSONArray>();
+
     private final MultiModel multiModel;
 
-    public LocalEnsemble(JSONObject ensemble, String storage, int max)
+    private JSONArray distribution = null;
+
+    public LocalEnsemble(JSONObject ensemble, String storage, Integer max)
             throws Exception {
         this(ensemble);
     }
 
     /**
-     * Constructor
+     * Constructor with an Ensemble reference
      * 
      * @param ensemble
      *            the json representation for the remote ensemble
@@ -69,7 +77,11 @@ public class LocalEnsemble {
         } else {
             this.ensembleId = (String) ensemble.get("resource");
             JSONArray modelsJson = (JSONArray) Utils.getJSONObject(ensemble,
-                    "object.models");
+                    "models");
+            distribution = (JSONArray) Utils.getJSONObject(ensemble,
+                    "distributions");
+
+
             int mn = modelsJson.size();
             modelsIds = new String[mn];
             for (int i = 0; i < mn; i++) {
@@ -83,7 +95,175 @@ public class LocalEnsemble {
         for (String id : modelsIds) {
             models.add(bigmlClient.getModel(id));
         }
+
         multiModel = new MultiModel(models);
+
+        calculateFields();
+    }
+
+    /**
+     * Constructor with an Ensemble reference and the number
+     *  of max models to use
+     *
+     * @param ensemble
+     *            the json representation for the remote ensemble
+     */
+    public LocalEnsemble(JSONObject ensemble, Integer maxModels) throws Exception {
+
+        if (ensemble.get("objects") != null) {
+            throw new IllegalArgumentException("Embedded objects unsupported");
+        } else {
+            this.ensembleId = (String) ensemble.get("resource");
+            JSONArray modelsJson = (JSONArray) Utils.getJSONObject(ensemble,
+                    "models");
+            distribution = (JSONArray) Utils.getJSONObject(ensemble,
+                    "distributions");
+
+            int mn = modelsJson.size();
+            modelsIds = new String[mn];
+            for (int i = 0; i < mn; i++) {
+                modelsIds[i] = (String) modelsJson.get(i);
+            }
+        }
+
+        BigMLClient bigmlClient = BigMLClient.getInstance();
+        models = new JSONArray();
+
+        for (String id : modelsIds) {
+            models.add(bigmlClient.getModel(id));
+        }
+        int numberOfModels = models.size();
+
+        if( maxModels != null && maxModels > 1 ) {
+            int[] items = Utils.getRange(0, numberOfModels, maxModels);
+            for (int item : items) {
+                JSONArray arrayOfModels = new JSONArray();
+                arrayOfModels.addAll(models.subList(item, item+maxModels));
+                models_split.add(arrayOfModels);
+            }
+        }
+
+        multiModel = new MultiModel(models);
+
+        calculateFields();
+    }
+
+    /**
+     * Constructor with a list of model references and the number
+     *  of max models to use
+     *
+     * @param modelsIds
+     *            the model/id of each model to be used in the ensemble
+     * @param maxModels
+     *            the maximum number of models we will use in the ensemble
+     *            null if we do not want a maxModels value
+     */
+    public LocalEnsemble(List modelsIds, Integer maxModels) throws Exception {
+        this.modelsIds = (String[]) modelsIds.toArray(new String[modelsIds.size()]);
+        BigMLClient bigmlClient = BigMLClient.getInstance();
+        models = new JSONArray();
+
+        for (String id : this.modelsIds) {
+            models.add(bigmlClient.getModel(id));
+        }
+
+        int numberOfModels = models.size();
+
+        if( maxModels != null && maxModels > 1 ) {
+            int[] items = Utils.getRange(0, numberOfModels, maxModels);
+            for (int item : items) {
+                JSONArray arrayOfModels = new JSONArray();
+                arrayOfModels.addAll(models.subList(item, item+maxModels));
+                models_split.add(arrayOfModels);
+            }
+        }
+
+        multiModel = new MultiModel(models);
+
+        calculateFields();
+    }
+
+
+    public List<JSONArray> getFieldImportanceData() {
+        Map<String, Double> fieldImportance = new HashMap<String, Double>();
+
+        boolean useDistribution = false;
+        List<JSONObject> importances = new ArrayList<JSONObject>();
+        if( distribution != null && distribution.size() > 0 )  {
+            useDistribution = true;
+            for (Object item : distribution) {
+                JSONObject itemObj = (JSONObject) item;
+                useDistribution &= itemObj.containsKey("importance");
+                if( !useDistribution )
+                    break;
+                else
+                    importances.add((JSONObject) itemObj.get("importance"));
+            }
+        }
+
+        if( useDistribution ) {
+            for (JSONObject importance : importances) {
+                for (Object fieldInfo : importance.keySet()) {
+                    JSONArray fieldInfoArr = (JSONArray) fieldInfo;
+                    String fieldId = (String) fieldInfoArr.get(0);
+                    if( !fieldImportance.containsKey(fieldId)) {
+                        fieldImportance.put(fieldId, 0.0);
+                        String fieldName = (String) ((JSONObject)
+                                fields.get(fieldId)).get("name");
+
+                        JSONObject fieldNameObj = new JSONObject();
+                        fieldNameObj.put("name", fieldName);
+                    }
+
+                    fieldImportance.put(fieldId, fieldImportance.get(fieldId) +
+                            ((Number)fieldInfoArr.get(1)).doubleValue());
+                }
+            }
+        } else {
+            for (Object model : models) {
+                JSONObject modelObj = (JSONObject) model;
+                JSONArray fieldImportanceInfo = (JSONArray) Utils.getJSONObject(modelObj,
+                        "object.model.importance");;
+                for (Object fieldInfo : fieldImportanceInfo) {
+                    JSONArray fieldInfoArr = (JSONArray) fieldInfo;
+                    String fieldId = (String) fieldInfoArr.get(0);
+
+                    if( !fieldImportance.containsKey(fieldId)) {
+                        fieldImportance.put(fieldId, 0.0);
+                        String fieldName = (String) ((JSONObject)
+                                fields.get(fieldId)).get("name");
+
+                        JSONObject fieldNameObj = new JSONObject();
+                        fieldNameObj.put("name", fieldName);
+                    }
+
+                    fieldImportance.put(fieldId, fieldImportance.get(fieldId) +
+                            ((Number)fieldInfoArr.get(1)).doubleValue());
+                }
+            }
+        }
+
+        for (String fieldName : fieldImportance.keySet()) {
+            fieldImportance.put(fieldName, fieldImportance.get(fieldName) / models.size());
+        }
+
+        List<JSONArray> fieldImportanceOrdered = new ArrayList<JSONArray>();
+        for (String fieldName : fieldImportance.keySet()) {
+            JSONArray fieldInfo = new JSONArray();
+            fieldInfo.add(fieldName);
+            fieldInfo.add(fieldImportance.get(fieldName));
+            fieldImportanceOrdered.add(fieldInfo);
+        }
+
+        Collections.sort(fieldImportanceOrdered, new Comparator<JSONArray>() {
+            @Override
+            public int compare(JSONArray jsonArray, JSONArray jsonArray2) {
+                return (((Number) jsonArray.get(1)).doubleValue() >
+                        ((Number) jsonArray2.get(1)).doubleValue() ? -1 : 1);
+            }
+        });
+
+        return fieldImportanceOrdered;
     }
 
     /**
@@ -91,18 +271,37 @@ public class LocalEnsemble {
      * from the union of fields in all models of the ensemble.
      */
     public JSONObject getFields() {
-        JSONObject result = new JSONObject();
+        return fields;
+    }
+
+    /**
+     * Accessor to the full list of fields used by this ensemble. It's obtained
+     * from the union of fields in all models of the ensemble.
+     */
+    public Map<String, String> getFieldNames() {
+        return fieldNames;
+    }
+
+    /**
+     * Calculates the full list of fields used by this ensemble. It's obtained
+     * from the union of fields in all models of the ensemble.
+     */
+    protected void  calculateFields() {
+        fields = new JSONObject();
+        fieldNames.clear();
+
         for (int i = 0; i < this.modelsIds.length; i++) {
             JSONObject model = (JSONObject) this.models.get(i);
             JSONObject fields = (JSONObject) Utils.getJSONObject(model,
                     "object.model.fields");
             for (Object k : fields.keySet()) {
-                if (null == result.get(k)) {
-                    result.put(k, fields.get(k));
+                if (null != fields.get(k)) {
+                    String fieldName = (String) ((JSONObject) fields.get(k)).get("name");
+                    this.fields.put(k, fields.get(k));
+                    this.fieldNames.put((String) k, fieldName);
                 }
             }
         }
-        return result;
     }
 
     /**
@@ -127,9 +326,19 @@ public class LocalEnsemble {
             withConfidence = false;
         }
 
-        MultiVote votes = this.multiModel.generateVotes(inputData, byName,
-                withConfidence);
-        return votes.combine(method, withConfidence);
+        JSONObject inputDataObj = (JSONObject) JSONValue.parse(inputData);
+        MultiVote votes = this.multiModel.generateVotes(inputDataObj, byName,
+                null, withConfidence);
+        return votes.combine(method, withConfidence, null, null, null, null, null);
+    }
+
+    /**
+     * Makes a prediction based on the prediction made by every model.
+     */
+    public Map<Object, Object> predict(final JSONObject inputData, Boolean byName,
+            EnsemblePredictionMethod method, Boolean withConfidence)
+            throws Exception {
+        return predict(inputData, byName, method.getCode(), withConfidence);
     }
 
     /**
@@ -154,9 +363,23 @@ public class LocalEnsemble {
             withConfidence = false;
         }
 
-        MultiVote votes = this.multiModel.generateVotes(inputData, byName,
-                withConfidence);
-        return votes.combine(method, withConfidence);
+
+        MultiVote votes = null;
+
+        if( models_split != null && !models_split.isEmpty() ) {
+            votes = new MultiVote();
+            for (JSONArray splitModels : models_split) {
+                MultiModel splitMultiModel = new MultiModel(splitModels);
+                MultiVote splitVotes = splitMultiModel.generateVotes(inputData,
+                        byName, null, withConfidence);
+                votes.extend(splitVotes);
+            }
+        } else {
+            votes = this.multiModel.generateVotes(inputData, byName,
+                    null, withConfidence);
+        }
+
+        return votes.combine(method, withConfidence, null, null, null, null, null);
     }
 
     /**
