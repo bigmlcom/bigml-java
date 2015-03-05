b4d7039d013a52c2aaf9464f5ae041a7e9f6b02d
Javier Alperte <alperte@bigml.com>
2015-03-05 08:59:21 -0300

    Add interface to allow encoding/decoding of predictions by the respective predictive resources

--- src/main/java/org/bigml/binding/PredictionConverter.java
+++ src/main/java/org/bigml/binding/PredictionConverter.java
@@ -1,11 +1,24 @@
 package org.bigml.binding;
 
+import java.util.Locale;
+
 /**
- * Created with IntelliJ IDEA.
+ * Any prediction resource must implement this interface to allow
+ * conversion of prediction values from string to its correct data type
+ *
  * User: xalperte
  * Date: 3/2/15
  * Time: 3:46 PM
- * To change this template use File | Settings | File Templates.
  */
 public interface PredictionConverter {
+
+    /**
+     * Given a prediction string, returns its value in the required type
+     *
+     * @param valueAsString the value to be converted to the Objective data type
+     * @param locale the current locale to use to parse values
+     * @return the prediction in its data type
+     */
+    Object toPrediction(String valueAsString, Locale locale);
+
 }
